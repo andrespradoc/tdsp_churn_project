c74431bb5d9dd70fd0edbfeedef01e88f1481407
andrespc <brayanpradoq@gmail.com>
2023-12-11 11:07:45 -0500

    feat: Se añade en scripts/evaluation el calculo de las metricas de evaluación del modelo en producción

--- scripts/evaluation/main.py
+++ scripts/evaluation/main.py
@@ -10,7 +10,11 @@ mlflow models serve -m 'models:/{model_name}/Production' -p {port} --env-manager
 
 import requests
 import time 
+import json
+from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay, f1_score, recall_score, precision_score, accuracy_score
 
+# Ejemplo de los valores que recibe la funcion de evaluacion
+"""
 data = [[3.601381662065445,
   -0.584935532922293,
   -1.5476529221334174,
@@ -35,17 +39,33 @@ data = [[3.601381662065445,
   0.4998636849042407,
   -0.35370363388816944,
   -0.1843703458003117]]
+"""
+
+"""
+true_labels = [0,1]
+"""
 
 def get_labels_predicts(data: list):
     try: 
         init = time.time()
         r = requests.post("http://localhost:8124/invocations", json={"inputs": data})
         final = time.time()
-        print(r.text, f'Tiempo de respuesta: {final-init}') ## por ejemplo retorna {"predictions": [0, 1]}  Tiempo de respuesta: 0.155    
+        print(r.text, f'Tiempo de respuesta: {final-init}') ## por ejemplo retorna {"predictions": [0, 1]}  Tiempo de respuesta: 0.155
+        return r.json()    
     except Exception as ex:
         print(ex)
 
-if __name__ == '__main__':
-    predicts = get_labels_predicts(data)
-    print(predicts)
-    
+def evaluation_model(data, true_labels):
+  predicts = get_labels_predicts(data)
+  f1_ev = f1_score(true_labels, predicts)
+  acc_ev = accuracy_score(true_labels, predicts)
+  precision_ev = precision_score(true_labels, predicts)
+  recall_ev = recall_score(true_labels, predicts)
+  print(f"accuracy: {acc_ev} \n precision: {precision_ev}\n recall: {recall_ev}\n f1_score: {f1_ev}")
+  cm = confusion_matrix(true_labels, predicts)
+  disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['no', 'yes'])
+  disp.plot()
+  
+  if __name__ == '__main__':
+      evaluation_model(data, 
+                       true_labels)
